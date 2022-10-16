4d15211a2767d5e66459a41b65c6c62393dd7255
Ambika Patidar <ambika.patidar.cse@gmail.com>
2022-10-17 00:00:20 +0530

    the pawn game solution using multiple classes

--- app/game.rb
+++ app/game.rb
@@ -9,52 +9,59 @@ class Game
 
   def start
     input = @input_file.parse
-    first_input = input[0].split(' ')
-    return puts 'Please use correct input' if first_input[0] != 'PLACE'
+    first_command = input[0].split(' ')
+    return puts 'Please use correct input' if first_command[0] != 'PLACE'
 
     board = Board.new
     board.construct
+
     File.write(@output_file.file, '')
+
     origin = [0, 0]
-    input.each do |data|
-      splited_data = data.split(' ')
-      case splited_data[0]
+    input.each do |row|
+      data = row.split(' ')
+      case data[0]
       when 'PLACE'
-        place_data = splited_data[1].split(',')
-        origin = key = [place_data[0], place_data[1]]
-        board.board[key.join(',')] = { facing: place_data[2], colour: place_data[-1] }
+        place_data = data[1].split(',')
+        origin = [place_data[0], place_data[1]]
+        board.board[origin.join(',')] = { facing: place_data[2], colour: place_data[-1] }
       when 'MOVE'
         from = board.board[origin.join(',')]
-        if from[:facing] == 'NORTH'
-          origin[1] = splited_data[1].present? ? origin[1].to_i + splited_data[1].to_i : origin[1].to_i + 1
-        elsif from[:facing] == 'EAST'
-          origin[0] = splited_data[1].present? ? origin[0].to_i + splited_data[1].to_i : origin[1].to_i + 1
-        elsif from[:facing] == 'WEST'
-          origin[0] = splited_data[1].present? ? splited_data[1].to_i - origin[0].to_i : origin[1].to_i - 1 if origin[1].to_i > 0
-        elsif from[:facing] == 'SOUTH'
-          origin[1] = splited_data[1].present? ? splited_data[1].to_i - origin[1].to_i : origin[1].to_i - 1 if origin[1].to_i > 0 
+
+        case from[:facing]
+        when 'NORTH'
+          origin[1] = data[1].present? ? origin[1].to_i + data[1].to_i : origin[1].to_i + 1
+        when 'EAST'
+          origin[0] = data[1].present? ? origin[0].to_i + data[1].to_i : origin[1].to_i + 1
+        when 'WEST'
+          origin[0] = data[1].present? && origin[1].to_i.positive? ? data[1].to_i - origin[0].to_i : origin[1].to_i - 1
+        when 'SOUTH'
+          origin[1] = data[1].present? && origin[1].to_i.positive? ? data[1].to_i - origin[1].to_i : origin[1].to_i - 1
         end
+
         board.board[origin.join(',')] = { facing: from[:facing], colour: from[:colour] }
       when 'LEFT'
         current_origin = board.board[origin.join(',')]
-        current_origin[:facing] = if current_origin[:facing] == 'NORTH'
+        current_origin[:facing] = case current_origin[:facing]
+                                  when 'NORTH'
                                     'WEST'
-                                  elsif current_origin[:facing] == 'SOUTH'
+                                  when 'SOUTH'
                                     'EAST'
-                                  elsif current_origin[:facing] == 'EAST'
+                                  when 'EAST'
                                     'NORTH'
-                                  elsif current_origin[:facing] == 'WEST'
+                                  when 'WEST'
                                     'SOUTH'
                                   end
       when 'RIGHT'
         current_origin = board.board[origin.join(',')]
-        current_origin[:facing] = if current_origin[:facing] == 'NORTH'
+        current_origin[:facing] = case current_origin[:facing]
+                                  when 'NORTH'
                                     'EAST'
-                                  elsif current_origin[:facing] == 'SOUTH'
+                                  when 'SOUTH'
                                     'WEST'
-                                  elsif current_origin[:facing] == 'EAST'
+                                  when 'EAST'
                                     'SOUTH'
-                                  elsif current_origin[:facing] == 'WEST'
+                                  when 'WEST'
                                     'NORTH'
                                   end
       when 'REPORT'
